74523133e8edffdd1b5b1ccda34751a004e453a6
Meycemabd <meycemelabed@gmail.com>
2025-05-03 15:46:24 +0200

    express server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
 // Server init 
+
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+
+app.get('/', (req, res) => {
+    res.send('Sfectorian!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port: ${PORT}`);
+});
+
